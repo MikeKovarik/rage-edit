36339480ccd672af64d1b51825e6c233dece32d0
OddMorning <OddMorning@ya.ru>
2018-12-06 12:44:21 +0300

    .delete()

--- src/delete.mjs
+++ src/delete.mjs
@@ -1,4 +1,4 @@
-import {execute, sanitizePath} from './util.mjs'
+import {execute, sanitizePath, getOptions, modeToArg} from './util.mjs'
 import {Registry} from './Registry.mjs'
 
 
@@ -12,40 +12,54 @@ Registry.prototype.clear = function(...args) {
 
 
 // Deletes the registry key and all of its subkeys and values.
-Registry.delete = function(path, name) {
-	if (name === undefined)
+Registry.delete = function(path, arg2, options) {
+	var type = typeof arg2
+	if (type === 'undefined')
 		return Registry.deleteKey(path)
+	else if (type === 'object')
+		return Registry.deleteKey(path, arg2)
 	else
-		return Registry.deleteValue(path, name)
+		return Registry.deleteValue(path, arg2, options)
 }
 
 // Deletes all values and subkeys of a key at given path and preserves the key itself.
-Registry.clear = async function(path) {
+Registry.clear = async function(path, options) {
 	await Promise.all([
-		Registry.clearValues(path),
-		Registry.clearKeys(path)
+		Registry.clearValues(path, options),
+		Registry.clearKeys(path, options)
 	])
 }
 
 
 // Deletes the registry key and all of its subkeys and values
-Registry.deleteKey = async function(path) {
+Registry.deleteKey = async function(path, options) {
 	// Allows both forwardslashes and backslashes
 	path = sanitizePath(path)
+	// Populate options with default values
+	options = getOptions(options)
 	// Note: Not returning, the output of the reg command saying 'operation completed successfully'.
 	//       Only await the process to finish. If any error occurs, the thrown error will bubble up.
-	await execute(['delete', path, '/f'])
+	await execute(['delete', path, '/f', modeToArg(options.mode)])
 }
 
 
 // Deletes single value entry inside the key (or default value if no name given).
-Registry.deleteValue = async function(path, name = Registry.DEFAULT) {
+Registry.deleteValue = async function(path, name, options) {
+	// Check if only path and options were passed
+	if (typeof name === 'object') {
+		options = name
+		name = Registry.DEFAULT
+	} else if (typeof name === 'undefined') {
+		name = Registry.DEFAULT
+	}
 	// Allows both forwardslashes and backslashes
 	path = sanitizePath(path)
+	// Populate options with default values
+	options = getOptions(options)
 	if (name === Registry.DEFAULT)
-		var args = ['delete', path, '/ve', '/f']
+		var args = ['delete', path, '/ve', '/f', modeToArg(options.mode)]
 	else
-		var args = ['delete', path, '/v', name, '/f']
+		var args = ['delete', path, '/v', name, '/f', modeToArg(options.mode)]
 	// Note: Not returning, the output of the reg command saying 'operation completed successfully'.
 	//       Only await the process to finish. If any error occurs, the thrown error will bubble up.
 	await execute(args)
@@ -54,21 +68,23 @@ Registry.deleteValue = async function(path, name = Registry.DEFAULT) {
 
 
 // Deletes all values inside the key. Preserves the key iteself and its subkeys.
-Registry.clearValues = async function(path) {
+Registry.clearValues = async function(path, options) {
 	// Allows both forwardslashes and backslashes
 	path = sanitizePath(path)
+	// Populate options with default values
+	options = getOptions(options)
 	// Note: Not returning, the output of the reg command saying 'operation completed successfully'.
 	//       Only await the process to finish. If any error occurs, the thrown error will bubble up.
-	await execute(['delete', path, '/va', '/f'])
+	await execute(['delete', path, '/va', '/f', modeToArg(options.mode)])
 }
 
 
 // Deletes all subkeys inside the key. Preserves the key's values.
-Registry.clearKeys = async function(path) {
+Registry.clearKeys = async function(path, options) {
 	// Allows both forwardslashes and backslashes
 	path = sanitizePath(path)
 	// Get list of keys and delete them one by one
-	var keys = await Registry.getKeys(path)
-	var promises = keys.map(key => Registry.delete(`${path}\\${key}`))
+	var keys = await Registry.getKeys(path, options)
+	var promises = keys.map(key => Registry.delete(`${path}\\${key}`, options))
 	await Promise.all(promises)
 }
